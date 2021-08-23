a733d32e6c457e559efec8715049e62917507bcc
andyarensman <86622025+andyarensman@users.noreply.github.com>
2021-08-23 12:08:13 -0700

    Middleware and MongoDB lessons completed

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
 const express = require('express');
+require('dotenv').config()
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const Blog = require('./models/blog');
 
 //express app
 const app = express();
 
+//connect to mongodb
+const dbURI = process.env.MONGO_URI;
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
 //register viwe engine
 app.set('view engine', 'ejs');
 
-//listen for requests
-app.listen(3000);
+//middleware and static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
 
+
+//routes
 app.get('/', (req, res) => {
-  const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'}
-  ];
-  res.render('index', { title: 'Home', blogs});
+  res.redirect('/blogs');
 })
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About'});
+})
+
+//blog routes
+
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt: -1 })
+    .then((result) => {
+      res.render('index', { title: 'All Blogs', blogs: result })
+    })
+    .catch(err => {
+      console.log(err);
+    })
 })
 
 app.get('/blogs/create', (req, res) => {
